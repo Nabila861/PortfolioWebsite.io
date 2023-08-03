8ca229cfa973a9cdf2e10dd1dc3586eaedac029a
Nabila861 <jahannabila690@gmail.com>
2023-08-04 00:03:12 +0600

    Updated a link

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -5,7 +5,7 @@ export default function Testimonials() {
     <div className="testimonials" id="testimonials">
       <div className="container">
         <div className="card">
-          <a href="https://ik.imagekit.io/emtbd/Project%20screenshots%20and%20Videos/Resume%20(1).pdf?updatedAt=1690964468273" target="_blank" rel="noopener noreferrer">
+          <a href="https://ik.imagekit.io/emtbd/Project%20screenshots%20and%20Videos/Resume.pdf?updatedAt=1691085760908" target="_blank" rel="noopener noreferrer">
             <img src="./assets/Nabila.png" alt="My resume"/>
           </a>
         </div>
